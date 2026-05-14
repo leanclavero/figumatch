9a20670274d73e6ab8cb7aec12b0281bb4caebe6
Pedro Clavero <pedro.clavero@mercadolibre.com>
2026-05-14 13:47:57 -0300

    debug: temporarily disable header to isolate connection reset issue

--- app/layout.tsx
+++ app/layout.tsx
@@ -44,7 +44,7 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col pb-20">
-        <Header />
+        {/* <Header /> */}
         <main className="flex-1 overflow-y-auto">
           {children}
         </main>
